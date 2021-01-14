a2ac3300e1d7760218cfad26efbdcaeba5fb1d80
Marshal Duncan <marshal.duncan@stu.boone.kyschools.us>
2021-01-14 14:11:59 +0000

    finished

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+var buttons = document.getElementsByClassName('colourButton');
 
+var heading;
+heading = document.getElementById('colourValue');
+heading.innerHTML = 'Hello world!';
+
+function setButtonColour(button, red, green, blue){
+    button.setAttribute('style',
+                        'background-color: rgb('+ red +','+ green +','+ blue +');'
+                       );
+}
+var answerMessage = document.getElementById('answer');
+function startGame() {
+ answerMessage.innerHTML = "";
+  var answerButton = Math.round(Math.random() * (buttons.length - 1));
+ 
+  for (var i = 0; i < buttons.length; i++) {
+ 
+    var red = makeColourValue();
+    var green = makeColourValue();
+    var blue = makeColourValue();
+ 
+    setButtonColour(buttons[i], red, green, blue);
+ 
+    if (i === answerButton) {
+      heading.innerHTML = `(${red}, ${green}, ${blue})`;
+    }
+ 
+    buttons[i].addEventListener('click', function(){
+        if (this === buttons[answerButton]) {
+            answerMessage.innerHTML = "Correct!";
+        } else {
+            answerMessage.innerHTML = "Wrong answer! Guess again!";
+        }
+    });
+ 
+  }
+ 
+}
+
+function makeColourValue(){
+    return Math.round(Math.random()*255);
+}
+document.getElementById('resetButton').addEventListener('click', startGame);
+myRandomNumber = Math.round(Math.random()*100);
+startGame();
